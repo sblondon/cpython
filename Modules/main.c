ce87f393d19b1d0b04fed40ff4e36428654665a5
Benjamin Peterson <benjamin@python.org>
2017-10-01 13:48:09 -0700

    bpo-31650: PEP 552 (Deterministic pycs) implementation
    Python now supports checking bytecode cache up-to-dateness with a hash of the
    source contents rather than volatile source metadata. See the PEP for details.
    
    While a fairly straightforward idea, quite a lot of code had to be modified due
    to the pervasiveness of pyc implementation details in the codebase. Changes in
    this commit include:
    
    - The core changes to importlib to understand how to read, validate, and
      regenerate hash-based pycs.
    
    - Support for generating hash-based pycs in py_compile and compileall.
    
    - Modifications to our siphash implementation to support passing a custom
      key. We then expose it to importlib through _imp.
    
    - Updates to all places in the interpreter, standard library, and tests that
      manually generate or parse pyc files to grok the new format.
    
    - Support in the interpreter command line code for long options like
      --check-hash-based-pycs.
    
    - Tests and documentation for all of the above.

--- Modules/main.c
+++ Modules/main.c
@@ -2,6 +2,7 @@
 
 #include "Python.h"
 #include "osdefs.h"
+#include "internal/import.h"
 #include "internal/pystate.h"
 
 #include <locale.h>
@@ -60,6 +61,11 @@ static int orig_argc;
 #define BASE_OPTS L"bBc:dEhiIJm:OqRsStuvVW:xX:?"
 
 #define PROGRAM_OPTS BASE_OPTS
+
+static const _PyOS_LongOption longoptions[] = {
+    {L"check-hash-based-pycs", 1, 0},
+    {NULL, 0, 0},
+};
 
 /* Short usage message (with %s for argv0) */
 static const char usage_line[] =
@@ -396,6 +402,7 @@ typedef struct {
     int quiet_flag;              /* Py_QuietFlag */
     int skip_first_line;         /* -x option */
     _Py_OptList xoptions;        /* -X options */
+    const char *check_hash_pycs_mode; /* --check-hash-based-pycs */
 } _Py_CommandLineDetails;
 
 /* Structure used by Py_Main() to pass data to subfunctions */
@@ -559,7 +566,9 @@ pymain_parse_cmdline_impl(_PyMain *pymain)
 
     _PyOS_ResetGetOpt();
     do {
-        int c = _PyOS_GetOpt(pymain->argc, pymain->argv, PROGRAM_OPTS);
+        int longindex = -1;
+        int c = _PyOS_GetOpt(pymain->argc, pymain->argv, PROGRAM_OPTS,
+                             longoptions, &longindex);
         if (c == EOF) {
             break;
         }
@@ -590,6 +599,22 @@ pymain_parse_cmdline_impl(_PyMain *pymain)
         }
 
         switch (c) {
+        case 0:
+            // Handle long option.
+            assert(longindex == 0); // Only one long option now.
+            if (!wcscmp(_PyOS_optarg, L"always")) {
+                cmdline->check_hash_pycs_mode = "always";
+            } else if (!wcscmp(_PyOS_optarg, L"never")) {
+                cmdline->check_hash_pycs_mode = "never";
+            } else if (!wcscmp(_PyOS_optarg, L"default")) {
+                cmdline->check_hash_pycs_mode = "default";
+            } else {
+                fprintf(stderr, "--check-hash-based-pycs must be one of "
+                        "'default', 'always', or 'never'\n");
+                return 1;
+            }
+            break;
+
         case 'b':
             cmdline->bytes_warning++;
             break;
@@ -1034,6 +1059,9 @@ pymain_set_global_config(_PyMain *pymain)
     maybe_set_flag(&Py_QuietFlag, cmdline->quiet_flag);
 
     maybe_set_flag(&Py_IgnoreEnvironmentFlag, pymain->core_config.ignore_environment);
+
+    if (cmdline->check_hash_pycs_mode)
+        _Py_CheckHashBasedPycsMode = cmdline->check_hash_pycs_mode;
 }
 
 
